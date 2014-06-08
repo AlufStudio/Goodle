fad49be95cbe419dd6fd414b79c89a1ebeccc102
Rama Zeta <zeattacker@yahoo.com>
2014-06-08 18:13:48 +0700

    Detail Kandidat

--- src/good/le/HomeScreen.java
+++ src/good/le/HomeScreen.java
@@ -2,7 +2,10 @@ package good.le;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 
 public class HomeScreen extends Activity {
 
@@ -10,6 +13,17 @@ public class HomeScreen extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_home_screen);
+		
+		Button btn1 = (Button)findViewById(R.id.button1);
+		btn1.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View arg0) {
+				// TODO Auto-generated method stub
+				Intent i = new Intent(HomeScreen.this, DetailKandidat.class);
+				startActivity(i);
+			}
+		});
 	}
 
 	@Override
